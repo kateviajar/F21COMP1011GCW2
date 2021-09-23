4112046d9622b913856a97282dece365b5b58ed8
kateviajar <egbert0802@hotmail.com>
2021-09-23 07:49:44 -0700

    Created chart view and generated a bar chart

--- src/main/java/com/example/f21comp1011gcw2/CameraSalesViewController.java
+++ src/main/java/com/example/f21comp1011gcw2/CameraSalesViewController.java
@@ -1,19 +1,42 @@
 package com.example.f21comp1011gcw2;
 
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.chart.BarChart;
 import javafx.scene.chart.CategoryAxis;
 import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.XYChart;
 
-public class CameraSalesViewController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class CameraSalesViewController implements Initializable {
 
     @FXML
-    private BarChart<String, Double> barChart;
+    private BarChart<String, Integer> barChart;
 
     @FXML
-    private CategoryAxis CameraAxis;
+    private CategoryAxis cameraAxis;
 
     @FXML
     private NumberAxis salesAxis;
 
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        //normally this information would come from a DB, and we would loop over it to create these data points
+        XYChart.Series<String, Integer> sales = new XYChart.Series<>();
+        sales.getData().add(new XYChart.Data<>("Camera 1", 67));
+        sales.getData().add(new XYChart.Data<>("Camera 2", 102));
+        sales.getData().add(new XYChart.Data<>("Camera 3", 38));
+        sales.getData().add(new XYChart.Data<>("Camera 4", 97));
+        sales.getData().add(new XYChart.Data<>("Camera 5", 55));
+
+        barChart.getData().addAll(sales);
+        barChart.setLegendVisible(false); // make the legend invisible
+
+        cameraAxis.setLabel("Camera Types");
+        salesAxis.setLabel("Sales Amount");
+
+    }
 }
